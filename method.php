34667ebdb52a35ef53e86c2b3c55ada6480ee473
root <root@localhost>
2017-08-24 14:12:54 +0000

    method

---
+++ method.php
@@ -0,0 +1,27 @@
+<?php
+
+$token = 'token';
+$text = 'Тест';
+$groups = '676';
+
+$config = array(
+  'owner_id' => $groups,
+  'from_group' => '1',
+  'message' => $text,
+  'mark_as_ads' => '0'
+);
+
+var_dump(send_request_params ('wall.post', $config));
+
+//////////ФУНКЦИИ//////////
+
+function send_request_params ($method, $params) {
+    globals $token;
+
+    $params['v'] = '5.63';
+    $params['access_token'] = $token;
+    
+    $method = 'https://api.vk.com/method/'.$method.'?';
+    
+    return json_decode(file_get_contents($method.http_build_query($params)));
+}
